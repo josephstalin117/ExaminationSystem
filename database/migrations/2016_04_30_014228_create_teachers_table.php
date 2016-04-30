8aa69b9b44df3842b2408e8872b6ea3a6d514300
LinJoseph <lyz2356002@gmail.com>
2016-04-30 11:51:48 +0800

    init user role table

--- database/migrations/2016_04_30_014228_create_teachers_table.php
+++ database/migrations/2016_04_30_014228_create_teachers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTeachersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role')->default('0')->comment('0admin,1teacher,2student');
-            $table->rememberToken();
+            $table->integer('uid')->comment('关联users表');
+            $table->string('nickname');
+            $table->integer('telephone');
+            $table->string('avatar');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('teachers');
     }
 }

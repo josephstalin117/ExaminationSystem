307e005e8a3fe7b4ea69c3195bfe062a7fce56cb
yale <460174373@qq.com>
2016-05-25 19:17:36 +0800

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">欢迎</div>
+                <div class="panel-heading">欢迎来到专业测试你的职业性格</div>
 
                 <div class="panel-body">
-                    我是坂本,有何贵干
+                    最精准的预测、最专业的评价，我们致力于你将来的职业生涯最合理的建议
                 </div>
             </div>
         </div>

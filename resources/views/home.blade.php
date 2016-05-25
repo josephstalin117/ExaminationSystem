c98eac68da5ca81df0fd8715d7047c66f5118c10
yale <460174373@qq.com>
2016-05-25 18:43:12 +0800

    test commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">控制台</div>
+                <div class="panel-heading">欢迎登陆</div>
                 @include('common.errors')
                 <div class="panel-body">
-                    欢迎登陆
+                    参加测试的人员请务必诚实、独立地回答问题，只有如此，才能得到有效的结果！
                 </div>
             </div>
         </div>
